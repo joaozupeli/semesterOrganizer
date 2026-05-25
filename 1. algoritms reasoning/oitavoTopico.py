5847e807d20f85710881a07696ba72299e95075b
Joao francisco Azevedo Zupeli <joaozupeli7@gmail.com>
2026-05-25 14:24:59 -0300

    fix: corrigindo algumas funcoes

--- 1. algoritms reasoning/oitavoTopico.py
+++ 1. algoritms reasoning/oitavoTopico.py
@@ -5,14 +5,52 @@
 #    1. max_():
 #       maior elemento de um vetor
 
-#    2. min_():
-#       menor elemento de um vetor
+# def max_number(vetor):
+#     numero_maximo = 0
+#     for i in vetor:
+#         if i > numero_maximo:
+#             numero_maximo = i
+#     return numero_maximo
+
+# print(max_number([1,2,3,4,5,6,7,8,9,10]))
+
+# #    2. min_():
+# #       menor elemento de um vetor
+
+# def min_number(vetor):
+#     numero_minimo = vetor[0]
+#     for i in vetor:
+#         if i < numero_minimo:
+#             numero_minimo = i
+#     return numero_minimo
+
+# print(min_number([1,2,3,4,5,6,7,8,9,10]))
+
 
 #    3. invert_():
 #       inverte um vetor
 
+# def invert_vetor(vet):
+#     vetor_novo = []
+#     t = len(vet)
+#     for i in range(t-1,-1, -1):
+#         vetor_novo.append(vet[i])
+#     return vetor_novo
+        
+# print(invert_vetor([1,2,3,4,5,6,7,8,9,10]))
+
 #    4. lower_():
 #       todos os caracteres para minúsculo
+
+def lower_case(frase):
+    vetor = []
+    for i in frase:
+        vetor.append(ord(i))
+    return vetor
+print(lower_case("DOIDO"))
+
+
+
 
 #    5. upper_():
 #       todos os caracteres para maiúsculo
@@ -20,16 +58,6 @@
 #    6. isdigit_():
 #       verifica se é um digito
 
-def isdigit_(inpt):
-    for elemento in inpt:
-        for char in elemento:
-            n_ascii = ord(char)
-            if ord('0') <= n_ascii <= ord('9'):
-                return True
-            else:
-                return False
-
-print(isdigit_(['100', '2', '3', '4', '543', '6', '7', '8', '9', '0']))
 
 #    7. ischar_():
 #       verifica se é um char
@@ -56,3 +84,15 @@ print(isdigit_(['100', '2', '3', '4', '543', '6', '7', '8', '9', '0']))
 #           [1,2,1,3,3,4,3,1,2,1,1] -> [[1,5], [2,2], [3,3], [4,1]]
 
 # 4. Crie uma função que valida um CPF:
+
+
+# def triple_number(numero):
+#     return numero * 3
+
+# print(triple_number(10))
+
+
+# def soma(a,b,c):
+#     return a + b + c
+
+# print(soma(10,20,30))
